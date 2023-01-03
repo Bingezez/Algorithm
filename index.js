9d1e965a4326057a9d53b639ccfe7be3f020ded0
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-03 12:21:57 +0200

    index

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const {detailReviewBubbleSort} = require('./algorithm/bubbleSort');
+const {linearSearch} = require('./algorithm/linearSearch');
 
-console.log(detailReviewBubbleSort([1,3,2,5,7,6,4]));
+
+// console.log(detailReviewBubbleSort([1,3,2,5,7,6,4]));
+console.log(linearSearch([1,3,2,5,7,6,4], 5));
+
